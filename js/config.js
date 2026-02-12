af8afae4e774dd8896ce589b9e7461a16c2e12d7
SurekaSM <surekasureka883@gmail.com>
2026-02-12 16:05:22 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,13 @@
 window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_iCbzLDDZu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5a5dmrl9cdrq8ccqv91jt6sk0j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
     api: {
-        invokeUrl: 'https://zsievbkdqg.execute-api.ap-south-1.amazonaws.com/sbi' // Replace with your API URL
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
